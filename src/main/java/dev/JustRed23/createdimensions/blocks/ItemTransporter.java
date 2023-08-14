1d3a5075126452aae83427d6c01092c2ad4251b2
JustRed23 <manuel.bormans@gmail.com>
2023-08-14 14:07:25 +0200

    Change item transporter model

--- src/main/java/dev/JustRed23/createdimensions/blocks/ItemTransporter.java
+++ src/main/java/dev/JustRed23/createdimensions/blocks/ItemTransporter.java
@@ -13,6 +13,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
@@ -91,5 +92,9 @@ public class ItemTransporter extends HorizontalDirectionalBlock implements IBE<I
     @Nullable
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
         return defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
+        return 5;
     }
 }
