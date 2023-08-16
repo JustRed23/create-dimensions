562e3407556be48c61d6487e2fb6bc292f945325
JustRed23 <manuel.bormans@gmail.com>
2023-08-16 21:14:40 +0200

    Fix comparator output with item transporters

--- src/main/java/dev/JustRed23/createdimensions/blocks/ItemTransporter.java
+++ src/main/java/dev/JustRed23/createdimensions/blocks/ItemTransporter.java
@@ -2,6 +2,7 @@ package dev.JustRed23.createdimensions.blocks;
 
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.foundation.item.ItemHelper;
 import dev.JustRed23.createdimensions.blocks.blockentities.ItemTransporterEntity;
 import dev.JustRed23.createdimensions.register.CDBlockEntities;
 import net.minecraft.core.BlockPos;
@@ -10,22 +11,19 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.CapabilityItemHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -105,7 +103,9 @@ public class ItemTransporter extends HorizontalDirectionalBlock implements IBE<I
 
     public int getAnalogOutputSignal(@NotNull BlockState pBlockState, @NotNull Level pLevel, @NotNull BlockPos pPos) {
         return getBlockEntityOptional(pLevel, pPos)
-                .map(be -> AbstractContainerMenu.getRedstoneSignalFromContainer(be.getSyncInventory()))
+                .map(vte -> vte.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY))
+                .map(lo -> lo.map(ItemHelper::calcRedstoneFromInventory)
+                        .orElse(0))
                 .orElse(0);
     }
 }
